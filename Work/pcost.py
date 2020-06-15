427bd7cb4cb1fb0b5a2a64d30714f5ea3b36d41a
lingram2020 <66635934+lingram2020@users.noreply.github.com>
2020-06-15 08:54:19 -0500

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -25,5 +25,83 @@ f.close()
 total = sum(total_list) # add values
 print('Total cost', total)
 
+# pcost.py
+#
+# Exercise 1.27
+import os
+import csv
+import sys
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    total_cost = 0.0
+    
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row_no, row in enumerate(rows, start=1):
+            record = dict(zip(header, row))
+            try:
+                shares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += shares*price
+            except ValueError:
+                print(f"Row {row_no}: Bad row: {row}")
+
+    return total_cost  # add values
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/missing.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
+
+# pcost.py
+#
+# Exercise 1.27
+# using read_portfolio() function from report.py
+import os
+import csv
+import sys
+import report
+
+def portfolio_cost(filename, select=['name','shares','price'], types=[str,int,float], has_headers=True):
+    '''
+    Computes the total cost (shares*price) of a portfolio file
+    '''
+    portfolio = report.read_portfolio(filename, select=select, types=types, has_headers=has_headers)
+    total_cost = 0.0
+    for stock in portfolio:
+        total_cost += stock['shares'] * stock['price']
+
+    return total_cost  # add values
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/missing.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
